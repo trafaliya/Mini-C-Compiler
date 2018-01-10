1c42c551082d4553820d4392da9c432d8d21f77b
trafaliya <trafaliya@gmail.com>
2018-01-10 16:02:05 +0530

    Create comment.c

---
+++ Testcases/comment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+//This is a commment                             Valid comment
+
+/* 
+This is also a comment                           Valid comment
+*/
+
+                                               //Invalid comment
+/*
+This comment has no end                                   
+}
