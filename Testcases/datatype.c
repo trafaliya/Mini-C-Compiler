3c5edef28a2fdd50a995c8e2710fe045d3d5e795
trafaliya <trafaliya@gmail.com>
2018-01-10 15:35:43 +0530

    Create datatype.c

---
+++ Testcases/datatype.c
@@ -0,0 +1,21 @@
+
+// Test case for data types, declarations and assignment statements
+
+#include <stdio.h>
+
+int main()
+{
+	char a;             //valid char assignment
+        int 5xyz;           //invalid identifier
+	char a[100];
+
+	int x=10;           //valid int assignment      
+        x=2*q;              //invalid assignment
+  
+        float z=30.0;       //valid float assignment
+        float y=25.0;
+
+        z=x+y;              //valid statement
+	return 0;
+
+}
