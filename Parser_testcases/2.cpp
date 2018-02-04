91f8d8f31f88e3aae07a1da91bfbbf8b145f5b1e
Siddhartha Chowdhuri <siddharthachowdhuri@gmail.com>
2018-02-04 18:42:32 +0530

    Add files via upload

---
+++ Parser_testcases/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// expected parenthesis.
+int main() {
+	int a = 10,b = 11,c = 13;
+	int x = (a - c + b;
+	cout << x << endl;	
+}
---
+++ Parser_testcases/3.cpp
@@ -0,0 +1,6 @@
+#include <iostream> 
+using namespace std;
+//normal code
+int main() {
+	cout << "Hello World\n"';
+}
