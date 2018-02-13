d4822a2049626165e3100de492b0682b332cfb8d
trafaliya <trafaliya@gmail.com>
2018-02-14 02:33:58 +0530

    Update datatype.c

--- Parser_testcases/datatype.c
+++ Parser_testcases/datatype.c
@@ -6,16 +6,16 @@
 int main()
 {
 	char a;             //valid char assignment
-	char a[100];        //valid char assignment
 
 	int 5xyz;           //invalid identifier
 	int x=10;           //valid int assignment
-
-
-    float z=30.0;       //valid float assignment
-	float y=25.10;      //invalid float assignment
-
-    z=x+y;              //valid statement
+        int y=10,z=5;
+	
+        z*=10;
+	y+=5;
+	x-=1;
+	
+        z=x+y;              //valid statement
 	return 0;
 
 }
